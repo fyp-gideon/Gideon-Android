01d33c28ea68237b96d595f5cca63bd35558c1f4
Abdul Wahab <50889251+abdulwahab019@users.noreply.github.com>
2020-03-19 18:46:12 +0500

    - Main Screen Updated (Partially)

--- app/src/main/java/com/example/gideon/MainScreen.java
+++ app/src/main/java/com/example/gideon/MainScreen.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.MediaSource;
@@ -20,11 +21,6 @@ import com.google.android.exoplayer2.util.Util;
 public class MainScreen extends AppCompatActivity {
 
     private Button signOutBtn;
-    private PlayerView liveFeedView;
-    private SimpleExoPlayer player;
-    private boolean playWhenReady = true;
-    private int currentWindow = 0;
-    private long playbackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +32,7 @@ public class MainScreen extends AppCompatActivity {
     }
 
     private void initializeComponents() {
-        signOutBtn = findViewById(R.id.signOutBtn);
-        liveFeedView = findViewById(R.id.livefeed);
-        startLiveFeed();
+
     }
 
     private void onListeners() {
@@ -49,78 +43,5 @@ public class MainScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    private void startLiveFeed(){
-        player = new SimpleExoPlayer.Builder(this).build();
-        liveFeedView.setPlayer(player);
-
-        Uri uri = Uri.parse("https://streamable.com/s/ju6gs/nxfheu");
-        MediaSource mediaSource = buildMediaSource(uri);
-
-        player.setPlayWhenReady(playWhenReady);
-        player.seekTo(currentWindow, playbackPosition);
-        player.prepare(mediaSource, false, false);
-    }
-
-    private MediaSource buildMediaSource(Uri uri) {
-        DataSource.Factory dataSourceFactory =
-                new DefaultDataSourceFactory(this, "admin");
-
-        return new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(uri);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (Util.SDK_INT >= 24) {
-            startLiveFeed();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        hideSystemUi();
-        if ((Util.SDK_INT < 24 || player == null)) {
-            startLiveFeed();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (Util.SDK_INT < 24) {
-            releasePlayer();
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (Util.SDK_INT >= 24) {
-            releasePlayer();
-        }
-    }
-
-    private void releasePlayer() {
-        if (player != null) {
-            playWhenReady = player.getPlayWhenReady();
-            playbackPosition = player.getCurrentPosition();
-            currentWindow = player.getCurrentWindowIndex();
-            player.release();
-            player = null;
-        }
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        liveFeedView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
