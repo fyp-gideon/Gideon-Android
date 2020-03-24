fcb2ea2e91930116fee624344c931551f0b62a5a
Abdul Wahab <50889251+abdulwahab019@users.noreply.github.com>
2020-03-24 14:53:34 +0500

    - Personal Setting Screen(UI) Added.

--- app/src/main/java/com/example/gideon/MainScreen.java
+++ app/src/main/java/com/example/gideon/MainScreen.java
@@ -2,25 +2,15 @@ package com.example.gideon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.VideoView;
-
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
-    private Button signOutBtn;
+    private Intent intent;
+    private TextView personalBtn, signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +22,30 @@ public class MainScreen extends AppCompatActivity {
     }
 
     private void initializeComponents() {
-
+        signOutBtn = findViewById(R.id.signOut);
+        personalBtn = findViewById(R.id.personal);
     }
 
     private void onListeners() {
         signOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainScreen.this, MainActivity.class);
+                intent = new Intent(MainScreen.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+        personalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(MainScreen.this, PersonalScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable Back Button.
     }
 }
